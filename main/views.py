75d90c845e1e3a74bb8ad36c5c91f438d8bf6167
ray <temi@temis-MacBook-Pro.local>
2022-11-15 22:48:03 -0800

    find name, see availability and purchase

--- main/views.py
+++ main/views.py
@@ -12,6 +12,7 @@ from django.core.mail import send_mail
 from datetime import datetime
 import datetime as dt
 
+from main.ray_bns import GetAddress, GetPrice
 
 
 
@@ -60,4 +61,50 @@ def CheckOutView(request):
 		context = {}
 		return render(request, "main/checkout.html", context )
 
+
+
+
+def FindView(request):
+
+	if request.method == "POST":
+		domain_name = request.POST.get("domain_name")
+
+		return HttpResponseRedirect(reverse("main:result", args=[domain_name,]))
+
+	else:
+
+		context = {}
+		return render(request, "main/find.html", context )
+
+
+
+def ResultView(request, domain_name):
+
+	if request.method == "POST":
+		pass
+
+	else:
+		name_address = GetAddress(domain_name)
+
+		if name_address == "0x0000000000000000000000000000000000000000":
+			available = True
+		else:
+			available = False
+		
+		name_price = GetPrice(domain_name)
+
+		context = {"domain_name": domain_name, "available": available, "name_price": name_price}
+		return render(request, "main/result.html", context )
+
+
+
+def BuyView(request, domain_name):
+
+	if request.method == "POST":
+		pass
+
+	else:
+
+		context = {"domain_name": domain_name}
+		return render(request, "main/buy.html", context )
 
